d7b417430d32d5b265843d87953593e3d00e1fe5
hyeonju0925 <hyeonju0925@naver.com>
2024-04-29 21:55:01 +0900

    feat: login page

--- lib/successPage.dart
+++ lib/successPage.dart
@@ -18,24 +18,24 @@ class _Main_PageState extends State<Main_Page> {
         body: Container(
           margin: const EdgeInsets.only(top: 50),
           padding: const EdgeInsets.all(10.0),
-          child: Column(
-              children: [
-                Row(
-                    children: [
-                      const SizedBox(width: 270),
-                      SaveCon(),
-                      const SizedBox(width: 1),
-                      AlarmCon(),
-                      const SizedBox(height: 20),
-                    ]
-                ),
-                SearchB(),
-                const SizedBox(height: 10),
-                Recent_text(),
-                Recent_Activity(),
-                Find_text(),
-
-              ]
+          child: SingleChildScrollView(
+            child: Column(children: [
+              Row(children: [
+                const SizedBox(width: 270),
+                SaveCon(),
+                const SizedBox(width: 1),
+                AlarmCon(),
+                const SizedBox(height: 20),
+              ]),
+              SearchB(),
+              const SizedBox(height: 10),
+              Recent_text(),
+              Recent_Activity(),
+              Find_text(),
+              Find_Activity(),
+              Realtime_text(),
+              Realtime_Activity(),
+            ]),
           ),
         ),
       ),
@@ -55,12 +55,6 @@ class _Main_PageState extends State<Main_Page> {
     );
   }
 
-  Widget Find_Activity(){
-    return SingleChildScrollView(
-
-    );
-  }
-
   Widget Find_text() {
     return TextFormField(
       decoration: const InputDecoration(
@@ -74,54 +68,123 @@ class _Main_PageState extends State<Main_Page> {
     );
   }
 
+  Widget Realtime_text() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        border: InputBorder.none,
+        labelText: '실시간 최근 올라온 책들',
+        labelStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget Find_Activity() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: RangeMaintainingScrollPhysics(),
+      child: Row(
+        children: [
+          Container(
+              child:
+                  Image.asset('image/picture_1.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_2.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_3.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_4.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_5.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+        ],
+      ),
+    );
+  }
+
   Widget Recent_Activity() {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
           Container(
-              child: Image.asset(
-                  'image/picture_1.png', height: 150, width: 150),
-              padding: EdgeInsets.all(10)
-          ),
+              child:
+                  Image.asset('image/picture_1.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
           Container(
-              child: Image.asset(
-                  'image/picture_2.png', height: 150, width: 150),
-              padding: EdgeInsets.all(10)
-          ),
+              child:
+                  Image.asset('image/picture_2.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
           Container(
-              child: Image.asset(
-                  'image/picture_3.png', height: 150, width: 150),
-              padding: EdgeInsets.all(10)
-          ),
+              child:
+                  Image.asset('image/picture_3.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
           Container(
-              child: Image.asset(
-                  'image/picture_4.png', height: 150, width: 150),
-              padding: EdgeInsets.all(10)
-          ),
+              child:
+                  Image.asset('image/picture_4.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
           Container(
-              child: Image.asset(
-                  'image/picture_5.png', height: 150, width: 150),
-              padding: EdgeInsets.all(10)
-          ),
+              child:
+                  Image.asset('image/picture_5.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+        ],
+      ),
+    );
+  }
+
+  Widget Realtime_Activity() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Container(
+              child:
+                  Image.asset('image/picture_1.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_2.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_3.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_4.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
+          Container(
+              child:
+                  Image.asset('image/picture_5.png', height: 150, width: 150),
+              padding: EdgeInsets.all(10)),
         ],
       ),
     );
   }
 
   Widget SaveCon() {
-    return IconButton(onPressed: () {
-      Navigator.push(
-        context, MaterialPageRoute(builder: (context) => SaveSpace()),
-      );
-    },
+    return IconButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SaveSpace()),
+          );
+        },
         icon: Icon(Icons.bookmark_outline_outlined));
   }
 
   Widget SearchB() {
-    return SearchBar(
-        leading: Icon(Icons.search)
-    );
+    return SearchBar(leading: Icon(Icons.search));
   }
 
   Widget SearchCon() {
