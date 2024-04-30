1fc44ae361d4be09912a9f16494a8d9b6101c978
hyeonju0925 <hyeonju0925@naver.com>
2024-04-30 20:24:21 +0900

    디자인 수정 및 새 파일 생성

--- lib/Alarm_space.dart
+++ lib/Alarm_space.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SaveSpace extends StatelessWidget{
+class AlarmSpace extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
--- lib/Book_SearchList.dart
+++ lib/Book_SearchList.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SaveSpace extends StatelessWidget{
+class Booklist extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
 
-      ),
     );
   }
 }
