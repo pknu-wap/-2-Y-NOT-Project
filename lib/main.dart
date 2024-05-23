23e8fee346c87264b27f5036be9ddbb4c9d303f0
InYoung05 <iinyoung052608@gmail.com>
2024-05-24 02:15:23 +0900

    Fix redundant character escape in email validation regex

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// main.dart
-
 import 'package:flutter/material.dart';
 import 'MyPage.dart'; // MyPage.dart 파일을 임포트합니다
 
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const MyPage(), // MyHomePage 위젯을 초기 화면으로 사용합니다
+      home: const MyPage(inputText: 'your_input_text_here'),      // MyHomePage 위젯을 초기 화면으로 사용합니다
     );
   }
 }
