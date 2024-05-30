0135ebcee5bda10d659c9a9aac6d035bf921ac80
InYoung05 <iinyoung052608@gmail.com>
2024-05-30 16:23:00 +0900

    Fix redundant character escape in email validation regex

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'DetailPage.dart';
+import 'package:flutter_01/Make_BookList.dart';
+import 'package:get/get.dart';
+import 'package:flutter_01/WishList.dart';
+import 'MyPage.dart'; // MyPage.dart 파일을 임포트합니다
+import 'package:flutter_01/loginPage.dart';
+import 'package:flutter_01/Make_BookList.dart';
+import 'package:flutter_01/About Chat/ChatList.dart';
+import 'package:get/get.dart';
+import 'package:flutter_01/successPage.dart';
+import 'package:flutter_01/Searchresult.dart';
+import 'package:flutter_01/Book_SearchList.dart';
+import 'package:flutter_01/About Chat/ChatList.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: DetailPage(), // 첫 화면으로 DetailPage를 사용합니다.
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter 엔진이 초기화될 때까지 기다림
+  await Firebase.initializeApp(); // Firebase 초기화
+  runApp(MyApp()); // MyApp 위젯을 실행
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter My Page',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/success': (context) => ChatListScreen(),//BookList(Searchresult: searchresult),
+      },
+      home: WishListForm(),
+    );
+  }
 }
