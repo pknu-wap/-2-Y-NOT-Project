e01b7762471bde45ea22c811c3c47446f308f04e
jjm1u <leejm1585@naver.com>
2024-06-01 21:22:27 +0900

    firestore book data on screen

--- lib/Book_db.dart
+++ lib/Book_db.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-class GetDataTest extends StatefulWidget {
-  const GetDataTest({Key? key}) : super(key: key);
+class GetBookDataTest extends StatefulWidget {
+  const GetBookDataTest({Key? key}) : super(key: key);
 
   @override
-  State<GetDataTest> createState() => GetDataTestState();
+  State<GetBookDataTest> createState() => _GetBookDataTestState();
 }
 
-class GetDataTestState extends State<GetDataTest> {
-  // 현재 로그인된 사용자의 uid를 가져옵니다.
-  String uid = FirebaseAuth.instance.currentUser!.uid;
-
-  // 사용자 정보를 비동기적으로 가져오는 함수입니다.
+class _GetBookDataTestState extends State<GetBookDataTest> {
+  // 특정 책의 정보를 비동기적으로 가져오는 함수입니다.
   // Firestore에서 데이터를 가져오는 동안 발생할 수 있는 예외를 처리합니다.
-  Future<Map<String, dynamic>?> getUserInfo() async {
+  Future<Map<String, dynamic>?> getBookInfo() async {
     try {
-      var result = await FirebaseFirestore.instance.collection('users').doc(uid).get();
+      var result = await FirebaseFirestore.instance.collection('book').doc('OF8n7W20F50k7SrdvQK0').get();
       return result.data() as Map<String, dynamic>?;
     } catch (e) {
       print(e); // 콘솔에 에러를 출력합니다.
@@ -29,22 +25,22 @@ class GetDataTestState extends State<GetDataTest> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: FutureBuilder<Map<String, dynamic>?>(
-            future: getUserInfo(),
+            future: getBookInfo(),
             builder: (context, snapshot) {
               // 데이터가 성공적으로 로드되었는지 확인합니다.
               if (snapshot.connectionState == ConnectionState.done) {
                 // 데이터가 있는지 확인합니다.
                 if (snapshot.hasData) {
                   var data = snapshot.data!;
-                  var username = data['username'] ?? 'No name';
-                  var email = data['email'] ?? 'No email';
+                  var bookTitle = data['BookTitle'] ?? 'No title';
+                  var complete = data['Complete'] ?? 'No status';
                   // 데이터를 화면에 표시합니다.
                   return Center(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(username),
-                        Text(email),
+                        Text(bookTitle),
+                        Text(complete.toString()), // Boolean 값인 경우 문자열로 변환해 표시합니다.
                       ],
                     ),
                   );
