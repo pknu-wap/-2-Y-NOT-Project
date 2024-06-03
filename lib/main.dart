c778c24d219bcd711485d7ef3c0ec8ece3935268
InYoung05 <iinyoung052608@gmail.com>
2024-06-03 16:25:13 +0900

    Merge pull request #59 from pknu-wap/feature/inyoung/mypage
    Fix redundant character escape in email validation regex

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_01/DetailPage.dart';
 import 'package:flutter_01/Make_BookList.dart';
 import 'package:get/get.dart';
 import 'package:flutter_01/WishList.dart';
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/success': (context) => ChatListScreen(),//BookList(Searchresult: searchresult),
       },
-      home: WishListForm(),
+      home: DetailPage(),
     );
   }
 }
