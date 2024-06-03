2ee99dc212d66b80511caf8890bea19c464e226c
InYoung05 <iinyoung052608@gmail.com>
2024-06-03 17:02:59 +0900

    Merge pull request #59 from pknu-wap/feature/inyoung/mypage
    Fix redundant character escape in email validation regex

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_01/SignUpPage.dart';
+import 'package:flutter_01/Make_BookList.dart';
+import 'package:get/get.dart';
+import 'package:flutter_01/WishList.dart';
+import 'MyPage.dart'; // MyPage.dart 파일을 임포트합니다
+import 'package:flutter_01/loginPage.dart';
+import 'package:flutter_01/Make_BookList.dart';
 import 'package:flutter_01/About Chat/ChatList.dart';
 import 'package:get/get.dart';
-import 'package:flutter_01/profile.dart';
-
+import 'package:flutter_01/successPage.dart';
+import 'package:flutter_01/Searchresult.dart';
+import 'package:flutter_01/Book_SearchList.dart';
+import 'package:flutter_01/About Chat/ChatList.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Flutter 엔진이 초기화될 때까지 기다림
   await Firebase.initializeApp(); // Firebase 초기화
-  Get.testMode = true;
   runApp(MyApp()); // MyApp 위젯을 실행
 }
 
@@ -27,7 +33,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/success': (context) => ChatListScreen(),//BookList(Searchresult: searchresult),
       },
-      home: ProfileFormPage(),
+      home: WishListForm(),
     );
   }
 }
