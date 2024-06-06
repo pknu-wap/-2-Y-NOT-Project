9082f7429d1d338273d0114f633930e4bf812861
hyeonju0925 <hyeonju0925@naver.com>
2024-06-07 03:58:20 +0900

    upgrade connect2

--- lib/Book_SearchList.dart
+++ lib/Book_SearchList.dart
@@ -7,7 +7,6 @@ import 'BookInfo.dart';
 
 void printlist(List<String>? parameter) {}
 
-void printlist(List<String>? parameter) {}
 
 class BookList extends StatelessWidget {
   final BookInfo Searchresult;
