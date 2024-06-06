208584af8aa6388d4658c196b8633a08525850aa
InYoung05 <iinyoung052608@gmail.com>
2024-06-07 01:35:18 +0900

    Merge pull request #59 from pknu-wap/feature/inyoung/mypage
    Fix redundant character escape in email validation regex

--- lib/WishList.dart
+++ lib/WishList.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_01/Book_SearchList.dart';
 import 'package:get/get.dart'; // GetX 패키지를 사용하는 경우 추가
 import 'package:flutter_01/successPage.dart';
+import 'package:flutter_01/Book_SearchList.dart';
 import 'package:flutter_01/MyPage.dart';
-import 'package:flutter_01/Searchresult.dart';
+
+class BookInfo {
+  final String title;
+  final String author;
+  final String publishing;
+
+  BookInfo({required this.title, required this.author, required this.publishing});
+}
 
 class BookList extends StatelessWidget {
   final BookInfo searchResult;
@@ -35,182 +42,124 @@ class _WishListFormState extends State<WishListForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
+      backgroundColor: Colors.white,
+      body: SafeArea(
         child: Column(
-        children: [
-        const SizedBox(height: 16)
-    ,
-    Row
-    (
-    children
-    :
-    [
-    const
-    SizedBox
-    (
-    width
-    :
-    16
-    )
-    ,
-    GestureDetector
-    (
-    child
-    :
-    const
-    Icon
-    (
-    Icons
-    .
-    arrow_back_ios_new
-    ,
-    color
-    :
-    Colors
-    .
-    grey
-    ,
-    size
-    :
-    24
-    )
-    ,
-    onTap
-    :
-    (
-    ) {
-    Navigator.pop(context);
-    // Handle back button
-    },
-    ),
-    const Expanded(child: SizedBox()),
-    const Text(
-    '프로필',
-    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-    ),
-    IconButton(
-    icon: Icon(Icons.notifications_none_outlined, color: Color(0xFFFE4D02), size: 32),
-    onPressed: () {
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => NotificationsPage()),
-    );
-    },
-    ),
-    ],
-    ),
-    body: SafeArea(
-    child: SingleChildScrollView(
-    child: Column(
-    children: [
-    const SizedBox(height: 8), // 상단바 위 여백을 늘리기 위해 추가
-    Container(
-    width: MediaQuery.of(context).size.width,
-    height: 1,
-    color: Colors.grey,
-    ),
-    Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: TextField(
-    controller: _itemController,
-    decoration: InputDecoration(
-    labelText: '항목 추가',
-    suffixIcon: IconButton(
-    icon: Icon(Icons.add),
-    onPressed: () {
-    _addItemToList();
-    },
-    ),
-    ),
-    ),
-    ),
-    ListView.builder(
-    shrinkWrap: true,
-    itemCount: _wishList.where((item) => item.isBookmarked).length,
-    itemBuilder: (BuildContext context, int index) {
-    final bookmarkedItems =
-    _wishList.where((item) => item.isBookmarked).toList();
-    return ListTile(
-    contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-    title: Center(
-    child: Text(
-    bookmarkedItems[index].name,
-    style: TextStyle(
-    color: Colors.black87, // 기본 텍스트 색상으로 수정
-    ),
-    ),
-    ),
-    trailing: IconButton(
-    icon: Icon(Icons.delete),
-    onPressed: () {
-    _removeItemFromList(bookmarkedItems[index]);
-    },
-    ),
-    leading: IconButton(
-    icon: Icon(bookmarkedItems[index].isBookmarked
-    ? Icons.bookmark
-        : Icons.bookmark_border),
-    onPressed: () {
-    _toggleBookmark(bookmarkedItems[index]);
-    },
-    ),
-    );
-    },
-    ),
-    ],
-    ),
-    ),
-    ),
-    bottomNavigationBar: BottomNavigationBar(
-    onTap: (int index) {
-    switch (index) {
-    case 0:
-    Navigator.push(
-    context, MaterialPageRoute(builder: (context) => MainPage()));
-    break;
-    case 1:
-    case 1:
-    case 1:
-    Navigator.push(
-    context,
-    MaterialPageRoute(
-    builder: (context) => BookList(
-    searchResult: BookInfo(
-    title: '', // 필요한 정보를 여기에 제공하세요
-    author: '',
-    publishing: '',
-    ),
-    ),
-    ),
-    );
-    break;
-    case 2:
-    Get.to(ChatScreen());
-    break;
-    case 3:
-    Navigator.push(
-    context, MaterialPageRoute(builder: (context) => MyPage()));
-    break;
-    }
-    },
-    items: [
-    BottomNavigationBarItem(
-    icon: Icon(
-    Icons.home_outlined,
-    ),
-    label: '홈'),
-    BottomNavigationBarItem(
-    icon: Icon(
-    Icons.add_outlined,
-    ),
-    label: '판매'),
-    BottomNavigationBarItem(icon: Icon(Icons.chat), label: '채팅'),
-    BottomNavigationBarItem(
-    icon: Icon(Icons.account_circle), label: '정보'),
-    ],
-    type: BottomNavigationBarType.fixed,
-    ),
+          children: [
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                const SizedBox(width: 16),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.arrow_back_ios_new,
+                    color: Colors.grey,
+                    size: 24,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                const Spacer(),
+                const Text(
+                  '위시 리스트',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                ),
+                const Spacer(),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.notifications_none_outlined,
+                    color: Color(0xFFFE4D02),
+                    size: 32,
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => NotificationsPage()),
+                    );
+                  },
+                ),
+                const SizedBox(width: 16),
+              ],
+            ),
+            const SizedBox(height: 8), // 상단바 위 여백을 늘리기 위해 추가
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 1,
+              color: Colors.grey,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _wishList.where((item) => item.isBookmarked).length,
+                itemBuilder: (BuildContext context, int index) {
+                  final bookmarkedItems = _wishList.where((item) => item.isBookmarked).toList();
+                  return ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                    title: Center(
+                      child: Text(
+                        bookmarkedItems[index].name,
+                        style: TextStyle(color: Colors.black87),
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        _removeItemFromList(bookmarkedItems[index]);
+                      },
+                    ),
+                    leading: IconButton(
+                      icon: Icon(bookmarkedItems[index].isBookmarked
+                          ? Icons.bookmark
+                          : Icons.bookmark_border),
+                      onPressed: () {
+                        _toggleBookmark(bookmarkedItems[index]);
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          switch (index) {
+            case 0:
+              Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
+              // Navigate to main page
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => BookList(
+                    searchResult: BookInfo(
+                      title: '', // 필요한 정보를 여기에 제공하세요
+                      author: '',
+                      publishing: '',
+                    ),
+                  ),
+                ),
+              );
+              break;
+            case 2:
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
+              break;
+            case 3:
+              Navigator.push(context, MaterialPageRoute(builder: (context) => MyPage()));
+              // Navigate to MyPage
+              break;
+          }
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.add_outlined), label: '판매'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: '채팅'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: '정보'),
+        ],
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 
@@ -250,8 +199,16 @@ class WishItem {
   WishItem({required this.name, this.isBookmarked = false});
 }
 
-void main() {
-  runApp(MaterialApp(
-    home: WishListForm(),
-  ));
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat Screen'),
+      ),
+      body: Center(
+        child: Text('Chat with others here'),
+      ),
+    );
+  }
 }
